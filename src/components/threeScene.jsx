cfc124e3c2759b734c2ac20d3c95bcc0d42b74bb
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-07-11 18:41:04 +0530

    adding the desktop model

--- src/components/threeScene.jsx
+++ src/components/threeScene.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls, Box } from "@react-three/drei";
+import ComputersCanvas from "./canvas/Computers";
 
 const ThreeScene=()=>{
     return(
-        <Canvas>
-            <ambientLight  />
-            <pointLight position={[10,10,10]}/>
-            <Box position={[0,0,0]} args={[2, 2, 2]}>
-                <meshStandardMaterial color="hotpink" attach='material' />
-            </Box>
-            <OrbitControls/>
-        </Canvas>
+        // <Canvas>
+        //     <ambientLight  />
+        //     <pointLight position={[10,10,10]}/>
+        //     <Box position={[0,0,0]} args={[2, 2, 2]}>
+        //         <meshStandardMaterial color="hotpink" attach='material' />
+        //     </Box>
+        //     <OrbitControls/>
+        // </Canvas>
+        <>
+            <ComputersCanvas/>
+        </>
     )
 }
 
