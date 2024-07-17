d11181cfa92751d22ff431ddac453a44db524b90
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-07-17 16:47:36 +0530

    adding the navbar ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import ThreeScene from './components/threeScene'
+import About from './components/About';
+import Contact from './components/Contact';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import Feedbacks from './components/Feedbacks';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Socials from './components/Socials';
+import Tech from './components/Tech';
+import Works from './components/works';
+import StarsCanvas from './components/canvas/Stars';
 
 
 const App=()=> {
 
   return (
-    <div className='row m-0 p-0 '>
-      <h1>Hello world</h1>
-      <ThreeScene/>
+    <div className='relative z-0 bg-primary'>
+      <div className="bg-hero-pattern bg-cover bg-no-rerpeat bg-center">
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <Education/>
+      <Socials/>
+      <div className='relative z-0'>
+        <Contact/>
+        <StarsCanvas/>
+
+      </div>
     </div>
   )
 }
