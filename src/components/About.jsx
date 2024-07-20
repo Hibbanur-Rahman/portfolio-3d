c55410c3da303b20b81481806afd1e2841c7c614
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-07-20 20:15:16 +0530

    adding the about section ui

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,60 @@
-const About=()=>{
-    return (
-        <>
-            <h1>About</h1>
-        </>
-    )
-}
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
-export default About;
+import { styles } from "../style";
+import { services } from "../constants/index";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.74)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer with a passion for creating innovative
+        and user-friendly applications. With a strong background in computer
+        science and a deep understanding of programming languages and
+        frameworks, I'm able to develop high-quality software that meets the
+        needs of users. I'm also experienced in working in a team environment
+        and have a proven track record of delivering projects on time and within
+        budget. I'm excited to bring my skills and experience to your team.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About,"about");
