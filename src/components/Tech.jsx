13fe155b314d3ba55880b605391ecb60114e865a
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-07-21 01:09:31 +0530

    adding the projects section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,20 @@
+import { SectionWrapper } from "../hoc";
+import { styles } from "../style";
+import { motion } from "framer-motion";
+import BallCanvas from './canvas/Ball';
+import { technologies } from "../constants";
 const Tech=()=>{
     return (
-        <>
-            <h1>Tech</h1>
-        </>
+        <div className="flex flex-row flex-wrap justify-center gap-10">
+            {
+                technologies.map((technology)=>(
+                    <div className="w-28 h-28" key={technology.name}>
+                        <BallCanvas icon={technology.icon}/>
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
-export default Tech;
+export default SectionWrapper(Tech,"tech");
